b007fdeb076a58a236a28f46259e35f17c740e58
dganon <ganon.danielle@gmail.com>
2025-02-23 21:50:47 -0700

    Implement hover effect

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 const container = document.getElementById("container");
-let gridCount = 16;
-// gridCount will be changed to user input
+let input = 16;
+const gridCount = input * input;
+//let flexBas = Math.round((100/input) * 10) / 10; // rounds to nearest tenth
+let flexBas = 100/input;
+// console.log(flexBas);
 
 function createDivs(){
     for (let i = 0; i < gridCount; i++){
         const newDiv = document.createElement("div");
         //newDiv.style.flex = "auto";
         //newDiv.style.flexBasis = "auto";
-        newDiv.textContent = "hi";
+        //newDiv.textContent = ".";
         newDiv.classList.add("square");
-        newDiv.style.flexBasis = "25%";
+        newDiv.style.flexBasis = flexBas + "%";
         container.append(newDiv);
+
+        newDiv.addEventListener ("mouseover", (event) => {
+            event.target.style.backgroundColor = "plum";
+        });
     }
 
     document.body.appendChild(container);
 }
 
 createDivs();
+ 
