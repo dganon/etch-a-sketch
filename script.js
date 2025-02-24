d90b049f091ef62b5020fd4f68e932ee60972d73
dganon <ganon.danielle@gmail.com>
2025-02-23 23:10:27 -0700

    Add user input validation

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 const container = document.getElementById("container");
-let input = 16;
-const gridCount = input * input;
+let userInput = 16;
+let gridCount = userInput * userInput;
 //let flexBas = Math.round((100/input) * 10) / 10; // rounds to nearest tenth
-let flexBas = 100/input;
+let flexBas = 100/userInput;
 // console.log(flexBas);
 
 let submitBtn = document.querySelector("button");
 let textField = document.getElementById("gridSize");
-let userInput;
 
 submitBtn.addEventListener("click", function(event){
     event.preventDefault();
     userInput = textField.value;
-    console.log(userInput);
+    gridCount = userInput * userInput;
+    flexBas = 100/userInput;
+    
+    if (userInput <= 100 && userInput > 0){
+        container.replaceChildren();
+        createDivs();
+    }
+    else{
+        alert("please choose a number between 1-100.");
+    }
 });
 
 createDivs();
@@ -20,9 +28,6 @@ createDivs();
 function createDivs(){
     for (let i = 0; i < gridCount; i++){
         const newDiv = document.createElement("div");
-        //newDiv.style.flex = "auto";
-        //newDiv.style.flexBasis = "auto";
-        //newDiv.textContent = ".";
         newDiv.classList.add("square");
         newDiv.style.flexBasis = flexBas + "%";
         container.append(newDiv);
@@ -33,6 +38,10 @@ function createDivs(){
     }
 
     document.body.appendChild(container);
+}
+
+function removeDiv(){
+
 }
 
 
